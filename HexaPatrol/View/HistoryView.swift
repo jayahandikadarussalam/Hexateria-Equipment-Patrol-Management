c761af52bcf525d21b9e74c2a459da410b6e25a3
jayahandikadarussalam <operationpprz@gmail.com>
2025-03-05 12:13:17 +0700

    Penambahan dan perubahan untuk menunjang HomeTabView dan HistoryView bahkan UserInfoView

---
+++ HexaPatrol/View/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  HexaPatrol
+//
+//  Created by Jaya Handika Darussalam on 04/03/25.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
